160205f6f2d6650f6dfb2f0ef85ac8fc3f07ef22
Roman <rrd@one-touch.ru>
2024-01-25 17:32:55 +0600

    fix rework change

--- database/migrations/2023_10_12_000000_create_pocket_token_table.php
+++ database/migrations/2023_10_12_000000_create_pocket_token_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pocket_token', function (Blueprint $table) {
             $table->id();
             $table->text('token')->nullable();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pocket_token');
     }
 };
