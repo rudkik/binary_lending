3257f30b79283530b6d6a874c91a330aebfe2065
Roman <rrd@one-touch.ru>
2024-01-29 12:30:46 +0600

    top  button register

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="/"><img src="/assets/img/logo.svg"></a>
-
+        <a href="https://po8.cash/register?utm_source=affiliate&a=7dWuLjF1iNh2H0&ac=po_signals&code=50START" class="btn btn-success">Register</a>
     </div>
 </nav>
